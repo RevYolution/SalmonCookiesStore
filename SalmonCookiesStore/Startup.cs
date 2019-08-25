41a7117b02c72748f72afd140b398a575f717fdb
Jon Rice <revyolution1120@gmail.com>
2019-08-25 11:47:10 -0700

    Starting to set up the admin dashboard for store management.

--- SalmonCookiesStore/Startup.cs
+++ SalmonCookiesStore/Startup.cs
@@ -7,19 +7,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SalmonCookiesStore.Data;
 
 namespace SalmonCookiesStore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,6 +41,17 @@ namespace SalmonCookiesStore
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            string ConnectionString = Environment.IsDevelopment()
+            ? Configuration.GetConnectionString("DefaultConnection")
+            : Configuration.GetConnectionString("ProductionConnection");
+
+            string UserConnectionString = Environment.IsDevelopment()
+                ? Configuration.GetConnectionString("UserConnection")
+                : Configuration.GetConnectionString("UserProductionConnection");
+
+
+            services.AddDbContext<SalmonStoresDbContext>(options => options.UseSqlServer(ConnectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
