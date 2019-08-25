428d3de5409819b06bc802901045653a30978f5f
Jon Rice <revyolution1120@gmail.com>
2019-08-25 15:49:18 -0700

    Setting up login and register page.

--- SalmonCookiesStore/Pages/Account/Login.cshtml.cs
+++ SalmonCookiesStore/Pages/Account/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,24 @@ namespace SalmonCookiesStore.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public LoginInput Input { get; set; }
         public void OnGet()
         {
+        }
+
+        /// <summary>
+        /// Nested class for requirements for user login. 
+        /// </summary>
+        public class LoginInput
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
         }
     }
 }
